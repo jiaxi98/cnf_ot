98a9e3749380cbde348bcdaa0b237774e427b247
jiaxi98 <jiaxi.zhao@u.nus.edu>
2025-04-03 07:39:12 +0800

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -8,7 +8,12 @@ with open(os.path.join(here, "requirements.txt"), encoding="utf-8") as f:
   requirements = f.read().splitlines()
 setup(
   name='neural-MFG',
-  version='0.0.1',
+  version='1.0.0',
   packages=find_packages(),
   install_requires=requirements,
 )
+
+# log
+# [1.0.0] - 2025-04-03
+# - support both conditional and unconditional generation
+# - add support for dimension reduction
