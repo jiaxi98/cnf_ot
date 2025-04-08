89a390f40f81f5e0ddff461129c8d0e4592f470a
jiaxi98 <jiaxi.zhao@u.nus.edu>
2025-04-08 23:18:08 +0800

    add symbolic scripts for nsf

---
+++ cnf_ot/models/nsf_symbol.py
@@ -0,0 +1,12 @@
+from sympy import symbols, diff, simplify
+
+
+x, xk, xk1, yk, yk1, deltak, deltak1 = symbols('x xk xk1 yk yk1, deltak, deltak1')
+sk = (yk1 - yk) / (xk1 - xk)
+xi = (x - xk) / (xk1 - xk)
+alpha = (yk1 - yk) * (sk * xi**2 + deltak * xi * (1 - xi))
+beta = sk + (deltak1 + deltak - 2 * sk) * xi * (1 - xi)
+f = yk + alpha / beta
+
+dfddeltak = simplify(diff(f, deltak))
+print(dfddeltak)
